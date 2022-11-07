0720263234378d7d213f35bacd09d3225ce5f2a4
SSlhx <sofiane.salih.eca@gmail.com>
2022-11-07 14:30:30 +0100

    hjklpmù

--- traitement.php
+++ traitement.php
@@ -1,21 +1,37 @@
 <?php
-        
-       session_start();
-       if (isset ($_SESSION['id'])){
+       
     
-       }
+       session_start();
 
-       else{
+    
+
         if(isset( $_POST ['id'],$_POST['pw'])){
 
             $id=$_POST ['id'];
             $pw=$_POST['pw'];
        
        if (isset ($_POST ['id']) && isset($_POST['pw'])){
-        if ($id=="Sofiane" && $pw=="112233"){
-            $_SESSION['id'] = $id;
+
+           try {
+        $db = new PDO("mysql:host=localhost;dbname=id",'root');
+       
+        echo 'Connected to database';
+        }
+    catch(PDOException $e)
+        {
+        echo $e->getMessage();
+        }
+       
+        if ($id !== "" && $pw !== "") {
+            $requete = "SELECT * FROM id where 
+              id = '".$id."' and password = '".$pw."' ";
+              $exec_requete = query($requete);
+             $reponse= fetch ($exec_requete);
+             $count = $reponse['count(*)'];
+             if($count!=0){
+             $_SESSION['id'] = $id;
             header('Location: principale.php');
-           
+             }
 
         }
              else
@@ -27,7 +43,7 @@
        
     }
 
-}
+
 
                  
                 
