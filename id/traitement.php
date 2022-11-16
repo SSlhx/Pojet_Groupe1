c5429b5c433b3719ec61767cb3a53b342f5af310
SSlhx <sofiane.salih.eca@gmail.com>
2022-11-16 12:47:29 +0100

    log

--- id/traitement.php
+++ id/traitement.php
@@ -6,46 +6,26 @@
     
 
         if(isset( $_POST ['id'],$_POST['pw'])){
-
+            include 'connexion.php';
             $id=$_POST ['id'];
             $pw=$_POST['pw'];
        
        if (isset ($_POST ['id']) && isset($_POST['pw'])){
-
-           try {
-        $db = new PDO("mysql:host=localhost;dbname=id",'root');
-       
-        echo 'Connected to database';
-        }
-    catch(PDOException $e)
+        if($id !== "" && $pw !== "")
         {
-        echo $e->getMessage();
-        }
-       
-        if ($id !== "" && $pw !== "") {
-            $requete = "SELECT * FROM id where 
-              id = '".$id."' and password = '".$pw."' ";
-              $exec_requete = query($requete);
-             $reponse= fetch ($exec_requete);
-             $count = $reponse['count(*)'];
-             if($count!=0){
-             $_SESSION['id'] = $id;
+        $requete = "SELECT count(*) FROM id where 
+        identifiant = '".$id."' and mot_de_passe = '".$pw."' ";
+        $exec_requete = mysqli_query($db,$requete);
+        $reponse = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+            if($count!=0) // nom d'utilisateur et mot de passe correctes
+            {
+            $_SESSION['id'] = $id;
             header('Location: principale.php');
-             }
-
-        }
-             else
-             {
-                header('Location: login.php'); // utilisateur ou mot de passe incorrect
-                echo "Utilisateur ou mot de passe incorrect";
             }
-       }
-       
-    }
-
-
-
-                 
-                
+           else {
+            header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
+           }
+    }}}
 
         ?>
