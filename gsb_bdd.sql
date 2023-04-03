1eef5a81e97c39842ffcb90699cff38930eda70f
SSlhx <sofiane.salih.eca@gmail.com>
2023-04-04 01:01:21 +0200

    Update gsb_bdd.sql

--- gsb_bdd.sql
+++ gsb_bdd.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 03 avr. 2023 à 12:26
--- Version du serveur : 5.7.36
--- Version de PHP : 7.4.26
+-- Hôte : localhost
+-- Généré le : lun. 03 avr. 2023 à 23:00
+-- Version du serveur :  10.5.16-MariaDB
+-- Version de PHP : 7.3.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `gsb_bdd`
+-- Base de données : `id20465321_db_gsb_g1`
 --
 
 -- --------------------------------------------------------
@@ -27,9 +28,8 @@ SET time_zone = "+00:00";
 -- Structure de la table `form_visite`
 --
 
-DROP TABLE IF EXISTS `form_visite`;
-CREATE TABLE IF NOT EXISTS `form_visite` (
-  `id_form` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `form_visite` (
+  `id_form` int(11) NOT NULL,
   `dateJ` date NOT NULL,
   `praticien` varchar(255) NOT NULL,
   `date` date NOT NULL,
@@ -37,10 +37,8 @@ CREATE TABLE IF NOT EXISTS `form_visite` (
   `bilan` text NOT NULL,
   `medicament` varchar(255) NOT NULL,
   `avis_visiteur` longtext NOT NULL,
-  `id_User` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_form`),
-  KEY `id_User` (`id_User`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
+  `id_User` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `form_visite`
@@ -50,7 +48,11 @@ INSERT INTO `form_visite` (`id_form`, `dateJ`, `praticien`, `date`, `motif`, `bi
 (25, '2023-03-13', 'M.Durant', '2023-03-13', 'mal de ventre', 'test test', 'Doliprane', 'idhjqxhflejdhqxljfbcedq', 7),
 (26, '2023-03-13', 'Dr.Sam', '2022-12-05', 'AUT', 'Mal de tête', 'doliprane', 'consultation agréable ', 7),
 (28, '2023-03-14', 'Dr.Sam', '2022-12-03', 'REL', 'test test', 'Smecta', 'ceci est un test', 10),
-(29, '2023-03-15', 'Dr.Axel', '2002-01-08', 'SOL', 'test', 'test', 'test\r\n', 10);
+(29, '2023-03-15', 'Dr.Axel', '2002-01-08', 'SOL', 'test', 'test', 'test\r\n', 10),
+(30, '2023-03-28', 'test', '2022-12-05', 'PRD', 'tfdgtefgdte', 'fzggdfg', 'grgrgrgrg', 10),
+(31, '2023-04-03', '', '2023-07-20', 'AUT', 'le praticien est intéressé.', 'doliprane', 'avis positif ', 10),
+(32, '2023-04-03', 'COLLIN', '2022-12-05', 'SOL', 'Souffre de maux de tête', 'Doliprane 1000', 'Bonne consultation', 23),
+(33, '2023-04-03', 'PROVENCHER', '2022-10-08', 'AUT', 'Souffre de crise de crise d\'épilepsie', 'Lamictal', 'un praticiens qui connais son métier', 24);
 
 -- --------------------------------------------------------
 
@@ -58,24 +60,25 @@ INSERT INTO `form_visite` (`id_form`, `dateJ`, `praticien`, `date`, `motif`, `bi
 -- Structure de la table `medicament`
 --
 
-DROP TABLE IF EXISTS `medicament`;
-CREATE TABLE IF NOT EXISTS `medicament` (
-  `num_medicament` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `medicament` (
+  `num_medicament` int(11) NOT NULL,
   `nom_commercial` varchar(255) NOT NULL,
   `famille_medicament` varchar(255) NOT NULL,
   `composition` varchar(255) NOT NULL,
   `effet_indesirable` varchar(255) NOT NULL,
-  `contre-indication` varchar(255) NOT NULL,
-  `prix_echantillon` int(11) NOT NULL,
-  PRIMARY KEY (`num_medicament`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `contre_indication` varchar(255) NOT NULL,
+  `prix_echantillon` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `medicament`
 --
 
-INSERT INTO `medicament` (`num_medicament`, `nom_commercial`, `famille_medicament`, `composition`, `effet_indesirable`, `contre-indication`, `prix_echantillon`) VALUES
-(1, 'test', 'test,test', 'test,test,test', 'test', 'test', 10);
+INSERT INTO `medicament` (`num_medicament`, `nom_commercial`, `famille_medicament`, `composition`, `effet_indesirable`, `contre_indication`, `prix_echantillon`) VALUES
+(3, 'Smecta', 'ADSORBANTS INTESTINAUX', 'Maltodextrine, saccharose, triacétate de glycéryle (E1518), dioxyde de silicium (E551), alcool éthylique, lécithine de soja (E322), parfum vanille', 'Aucun', 'Si patients présentant une intolérance au fructose, un syndrome de malabsorption du glucose et du galactose ou un déficit en sucrase/isomaltase', '4,39 euro'),
+(2, 'Doliprane 1000', 'paracétamol', 'sodium (409 mg/cp). Excipients : povidone, amidon prégélatinisé, carboxyméthylamidon sodique (type A), talc, stéarate de magnésium, hydroxypropylcellulose, hypromellose, macrogol 6000', 'diminution globule blanc', 'allergie au paracétamol ou à l\'un des autres composants contenus dans ce médicament', '2,00 euro'),
+(4, 'Spasfon', 'Antispasmodiques musculotropes', 'Phloroglucinol hydraté,Triméthylphloroglucinol', 'Aucun', ' si vous êtes allergique au phloroglucinol, au triméthylphloroglucinol ou à l\'un des autres composants contenus dans ce médicament', '3,17 euro'),
+(5, 'Lamictal', ' anti-épileptiques', 'Lamotrigine', 'Maux de tête, troubles de la vision, tremblements, somnolence, agitation, fatigue, vertiges, troubles digestifs, augmentation paradoxale de la fréquence des crises d\'épilepsie', 'Ne jamais donné à des personnes de moins de 18 ans pour traiter des troubles bipolaire', '4,73 euro');
 
 -- --------------------------------------------------------
 
@@ -83,27 +86,30 @@ INSERT INTO `medicament` (`num_medicament`, `nom_commercial`, `famille_medicamen
 -- Structure de la table `practiciens`
 --
 
-DROP TABLE IF EXISTS `practiciens`;
-CREATE TABLE IF NOT EXISTS `practiciens` (
-  `Numero_praticiens` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `practiciens` (
+  `Numero_praticiens` int(11) NOT NULL,
   `Nom_praticiens` varchar(255) DEFAULT NULL,
   `Prenom_praticiens` varchar(255) NOT NULL,
   `Adresse_praticiens` varchar(255) NOT NULL,
   `CP_praticiens` decimal(5,0) NOT NULL,
   `Ville_praticiens` varchar(255) NOT NULL,
-  `region_practiciens` varchar(255) NOT NULL,
-  `num_compte` int(11) NOT NULL,
-  PRIMARY KEY (`Numero_praticiens`),
-  KEY `num_compte` (`num_compte`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `region_practiciens` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `practiciens`
 --
 
-INSERT INTO `practiciens` (`Numero_praticiens`, `Nom_praticiens`, `Prenom_praticiens`, `Adresse_praticiens`, `CP_praticiens`, `Ville_praticiens`, `region_practiciens`, `num_compte`) VALUES
-(5, 'Richard', 'Sofiane', '51 rue du 8 Mai 1945, 51', '73410', 'Albens', 'Grand Est', 0),
-(4, 'Salih', 'Sofiane', '51 rue du 8 Mai 1945, 51', '73410', 'Albens', 'Auvergne-Rhône-Alpes', 0);
+INSERT INTO `practiciens` (`Numero_praticiens`, `Nom_praticiens`, `Prenom_praticiens`, `Adresse_praticiens`, `CP_praticiens`, `Ville_praticiens`, `region_practiciens`) VALUES
+(6, 'DECIANTS', 'Marine', '35 Rue de Marseille', 69000, 'Lyon', 'Rhone-Alpes'),
+(7, 'D\'Aoust', 'Théodore', '15, rue du Paillle en queue', 69000, 'Lyon', 'Rhone-Alpes'),
+(8, 'COLLIN', 'Roxanne', '43, rue du Clair Bocage', 69000, 'Lyon', 'Rhones-Alpes'),
+(9, 'ECHEVERRI', 'Donat', '69, avenue Voltaire', 69000, 'Lyon', 'Rhone-Alpes'),
+(10, 'BLER', 'Isaac', '27, rue du Clair Bocage', 69000, 'Lyon', 'Rhone-Alpes'),
+(11, 'PROVENCHER', 'Saville', '93, rue de l\'Epeule', 69000, 'Lyon', 'Rhone-Alpes'),
+(12, 'BOURQUE', 'Dielle', '87, avenue de l\'Amandier', 69000, 'Lyon', 'Rhone-Alpes'),
+(13, 'BELLEMARE', 'Verrill', '22, boulevard d\'Alsace', 0, 'Lyon', '69000'),
+(14, 'ABATTI', 'Taryll', '97 rue ferdinant buisson', 69000, 'Lyon', 'Rhone-Alpes');
 
 -- --------------------------------------------------------
 
@@ -111,10 +117,8 @@ INSERT INTO `practiciens` (`Numero_praticiens`, `Nom_praticiens`, `Prenom_pratic
 -- Structure de la table `region`
 --
 
-DROP TABLE IF EXISTS `region`;
-CREATE TABLE IF NOT EXISTS `region` (
-  `departement` varchar(255) NOT NULL,
-  PRIMARY KEY (`departement`)
+CREATE TABLE `region` (
+  `departement` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -142,18 +146,15 @@ INSERT INTO `region` (`departement`) VALUES
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_User` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id_User` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `type` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
-  `departement` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_User`),
-  KEY `departement` (`departement`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+  `departement` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `users`
@@ -167,7 +168,76 @@ INSERT INTO `users` (`id_User`, `username`, `type`, `password`, `nom`, `prenom`,
 (15, 'Sofianesalih4@gmail.com', 'delegue', '*846AEC788124A4D732D51692E35E9DE488607F86', '', '', ''),
 (16, 'yous', 'delegue', '35a9e381b1a27567549b5f8a6f783c167ebf809f1c4d6a9e367240484d8ce281', '', '', ''),
 (18, 'root', 'delegue', '*81F5E21E35407D884A6CD4A731AEBFB6AF209E1B', '', '', ''),
-(20, 'test', 'visiteur', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 's', 'o', 'Hauts-de-France');
+(20, 'test', 'visiteur', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 's', 'o', 'Hauts-de-France'),
+(21, 'paul', 'visiteur', '35e1d1aeed3f7179b02a0dfde8f4e826e191649ee2acfd6da6b2ce7a12aa0f8b', 'durant', 'paul', 'Auvergne-Rhône-Alpes'),
+(22, 'delegue', 'delegue', '789', '', '', ''),
+(23, 'dupont', 'visiteur', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'Dupont', 'Jean', 'Auvergne-Rhône-Alpes'),
+(24, 'alexandre', 'delegue', 'b3a8e0e1f9ab1bfe3a36f231f676f78bb30a519d2b21e6c530c0eee8ebb4a5d0', 'Richard', 'Alexandre', 'Auvergne-Rhône-Alpes'),
+(25, 'rick', 'responsable', '35a9e381b1a27567549b5f8a6f783c167ebf809f1c4d6a9e367240484d8ce281', 'Sanchez', 'Rick', 'Auvergne-Rhône-Alpes');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `form_visite`
+--
+ALTER TABLE `form_visite`
+  ADD PRIMARY KEY (`id_form`),
+  ADD KEY `id_User` (`id_User`);
+
+--
+-- Index pour la table `medicament`
+--
+ALTER TABLE `medicament`
+  ADD PRIMARY KEY (`num_medicament`);
+
+--
+-- Index pour la table `practiciens`
+--
+ALTER TABLE `practiciens`
+  ADD PRIMARY KEY (`Numero_praticiens`);
+
+--
+-- Index pour la table `region`
+--
+ALTER TABLE `region`
+  ADD PRIMARY KEY (`departement`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_User`),
+  ADD KEY `departement` (`departement`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `form_visite`
+--
+ALTER TABLE `form_visite`
+  MODIFY `id_form` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT pour la table `medicament`
+--
+ALTER TABLE `medicament`
+  MODIFY `num_medicament` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `practiciens`
+--
+ALTER TABLE `practiciens`
+  MODIFY `Numero_praticiens` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_User` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Contraintes pour les tables déchargées
